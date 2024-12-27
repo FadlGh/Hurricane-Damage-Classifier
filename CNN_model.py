350da5e3269e8b5191c906b4e85a48bb2042fc0d
Fadl Ghaddar <fadl2009gh@gmail.com>
2024-12-27 17:05:58 +0200

    added l2 regularization to limit overfitting

--- CNN_model.py
+++ CNN_model.py
@@ -2,11 +2,14 @@ import numpy as np
 import pickle
 from tf_keras.models import Sequential
 from tf_keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tf_keras.regularizers import l2
+import numpy as np
+import pickle
 
 X = []
 y = []
 
-for i in range(16): # Edit number based on how many chunks you have
+for i in range(16):  # Edit number based on how many chunks you have
     with open(f'X_chunk_{i}.pickle', 'rb') as f:
         X_chunk = pickle.load(f)
     with open(f'y_chunk_{i}.pickle', 'rb') as f:
@@ -23,22 +26,24 @@ X = X.reshape(-1, 256, 256, 1)
 print(len(X))
 
 model = Sequential([
-    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 1)),
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 1),
+           kernel_regularizer=l2(0.01)),
     MaxPooling2D(pool_size=(2, 2)),
     Dropout(0.25),
 
-    Conv2D(32, kernel_size=(3, 3), activation='relu'),
+    Conv2D(32, kernel_size=(3, 3), activation='relu', 
+           kernel_regularizer=l2(0.01)),
     MaxPooling2D(pool_size=(2, 2)),
     Dropout(0.25),
 
     Flatten(),
-    Dense(64, activation='relu'),
+    Dense(64, activation='relu', kernel_regularizer=l2(0.01)),
     Dropout(0.5),
-    Dense(1, activation='sigmoid') 
+    Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01))
 ])
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.15)
+model.fit(X, y, batch_size=32, epochs=7, validation_split=0.15)
